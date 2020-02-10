d80b7c5185e56019af1409f818377af11ce510c9
github username <github email address>
2020-02-10 12:16:34 -0800

    merge backend

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ console.log(request);
 // B0qhxT_TykOaYCEUGRvRrAuM
 
 const key = "&key=AIzaSyClWK6raBj4KMLWAwnX8KDT-EXU0HgdoRQ";
-
+const url = "https://www.googleapis.com/youtube/v3/activities?part=snippet,contentDetails&channelId="
+const songChannel = ["UC0C-w0YjGpqDXGB8IHb662A","UCANLZYMidaCbLQFWXBC95Jg"]//[Ed Sheeran(5),Taylor Swift(5)]
 
 document.addEventListener("DOMContentLoaded", function(){
-     fetchData();
+    songChannel.forEach(channel => {fetchData(channel)}) 
 })
-function fetchData() 
+
+function fetchData(channelId) 
 {
     //songs
-    fetch("https://www.googleapis.com/youtube/v3/activities?part=snippet,contentDetails&channelId=UC0C-w0YjGpqDXGB8IHb662A&key=AIzaSyClWK6raBj4KMLWAwnX8KDT-EXU0HgdoRQ") //Ed Sheeran
+    fetch(url+channelId+key) 
     .then(resp => resp.json())
     .then(data => {
-        console.log(data)
+        // console.log(data)
         console.log(listOfId(data))
 })
 }
@@ -26,8 +28,9 @@ function listOfId(songs)
 {
     let songList = [];
     for (let song in songs.items) 
-    {   if (songs.items[song].contentDetails.upload.videoId)
-        {
+    {   if (songs.items[song].contentDetails.upload)
+        {   
+            console.log(songs.items[song].contentDetails.upload.videoId)
             let songUrl = "https://www.youtube.com/watch?v="+songs.items[song].contentDetails.upload.videoId
             songList.push(songUrl);
             // console.log(songList)
